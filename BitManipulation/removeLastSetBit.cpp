2118e29d70c5e26e089698324c1c753a94e44f50
Kaustuv <anjan.agtkol@gmail.com>
2025-04-21 21:56:11 +0530

    remove last set bit by n & n-1

---
+++ BitManipulation/removeLastSetBit.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+// to remove last set bit we need to use AND operator
+// to remember AND -> ' bit1 * bit2 '
+// we need to AND n with n-1
+//       #          <- 1st set bit here
+// 16 -> 1 0 0 0 0
+// 15 -> 0 1 1 1 1
+// 16 & 15 = 0 0 0 0 0 <- first set bit removed
+
+int n = 15; // 15 = 1 1 1 1
+int main(){
+    int ans = n & (n-1);
+    cout << ans << endl;
+    return 0;
+}
