9e56f967a6c3cbf62818043e9f9f8db3ed7d97d3
Kaustuv <anjan.agtkol@gmail.com>
2025-07-31 17:11:58 +0530

    shortest common super sequence

--- DP/ShortestComSuperSeq.cpp
+++ DP/ShortestComSuperSeq.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-// Step 1: Build LCS DP table
+// Step 1: Building LCS DP table
 vector<vector<int>> lcsTable(const string& X, const string& Y) {
     int m = X.size(), n = Y.size();
     vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
@@ -19,7 +21,7 @@ vector<vector<int>> lcsTable(const string& X, const string& Y) {
     return dp;
 }
 
-// Step 2: Build the SCS using the LCS table
+// Step 2: Building the SCS using the LCS table
 string printSCS(const string& X, const string& Y) {
     int i = X.size(), j = Y.size();
     vector<vector<int>> dp = lcsTable(X, Y);
@@ -28,29 +30,35 @@ string printSCS(const string& X, const string& Y) {
     while (i > 0 && j > 0) {
         if (X[i - 1] == Y[j - 1]) {
             // common character, part of LCS
-            result = X[i - 1] + result;
+            result.push_back(X[i - 1]);
             i--;
             j--;
         } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            result = X[i - 1] + result;
+            result.push_back(X[i - 1]);
             i--;
         } else {
-            result = Y[j - 1] + result;
+            result.push_back(Y[j - 1]);
             j--;
         }
     }
 
     // Add remaining characters
-    while (i > 0) result = X[i - 1] + result, i--;
-    while (j > 0) result = Y[j - 1] + result, j--;
+    while (i > 0) {
+        result.push_back(X[i - 1]);
+        i--;// we are adding the remaining characters from X and after that the result
+    }
+    while (j > 0) {
+        result.push_back(Y[j - 1]);
+        j--;
+    }
+    reverse(result.begin(), result.end()); // reverse to get the correct order
 
     return result;
 }
 
-// Driver
 int main() {
     string str1 = "AGGTAB", str2 = "GXTXAYB";
     string scs = printSCS(str1, str2);
-    cout << "SCS: " << scs << endl;
+    cout << "Shortest Common Supersequence: " << scs << endl;
     return 0;
 }
